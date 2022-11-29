fedbf794052830e2d06cd1a918c52f4db16124aa
AHMED RAMADAN <ahmedramadan5452332@gmail.com>
2022-11-29 23:26:00 +0200

    no thing

--- project x/main.py
+++ project x/main.py
@@ -1,21 +1,26 @@
+while True:
+    string = str(input("Enter String :").strip())
+    index = int(input("Enter Number :"))
 
-string = str(input("Enter String :").strip())
-index = int(input("Enter Number :"))
-
-
-def remove(string, index):
-    for A in range(len(string)):
-        if A == index:
-            string = string.replace(string[index], "")
-            return (string)
+    def remove(string, index):
+        for A in range(len(string)):
+            if A == index:
+                string = string.replace(string[index], "")
+                return (string)
     if index < 0:
         print("Please Enter Positive Number")
         if string == "":
-            # we use this condition to the user to do not leave it empty
+         # we use this condition to the user to do not leave it empty
             print(f"you shouldn't enter non empty string..  ")
         elif index > len(string):
-            # we use this condition to print this message for user when he input bigger index than string
+         # we use this condition to print this message for user when he input bigger index than string
             print("out of range please enter index to remove from your string :")
     else:
         print(
             f"your string after removing the number:{remove(string,index).upper()}")
+        ans = input("you can repeat this process  (y or n): ".strip().upper())
+        if ans == "Y":
+            continue
+        elif ans == "N":
+            print("Thank's For Using This Program")
+            break
