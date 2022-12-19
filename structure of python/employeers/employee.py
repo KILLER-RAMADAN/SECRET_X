abcd1fe4465111e796e1bb97ada4cce9deaabbc0
ahmed ramadan <ahmedramadan5452332@gamil.com>
2022-12-19 07:25:45 +0200

    new ubdates in employeers

--- structure of python/employeers/employee.py
+++ structure of python/employeers/employee.py
@@ -15,20 +15,31 @@ class employee:
 
 
 
-    def  calculate_age(self):
-      return employee.now-self.birthday
+  
     
     
     def get_name(self):
-      return self.__name
+      print(self.name)
     
     def calculate_gpa(self):
       if self.gpa==0:
         print("false")
       elif self.gpa>0:
         print("true")
-        
+    def bouns(self):
+     if self.age==60:
+       self.salary=self.salary+500
+       print(self.salary)
       
+      
+     else:
+      print("no increased salary your salary is "+ str(self.salary))
+        
+
+em=employee("ahmed",6,"m110",True,False,"egypt",10,20000,2003,3,21510857)
+em.get_name()
+
+em.bouns()
 
 # def bouns(self):
 # if self.age==60:
@@ -36,3 +47,15 @@ class employee:
 #     print("your salary is increased>>"+ str(self.sallary+500))
 # else:
 #     print("no increased salary "+ str(self.sallary+500))
+
+
+class student:
+    energy = 100
+
+    def eat_apple(self):
+        self.energy += 10
+
+
+player1 = student()
+player1.eat_apple()
+print(player1.energy)
