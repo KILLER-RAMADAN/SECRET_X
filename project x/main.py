a575350bb000c69c87824d2c2ac234ac11e204d1
ahmed ramadan <ahmedramadan5452332@gamil.com>
2022-12-22 06:15:55 +0200

    adding solution 65

--- project x/main.py
+++ project x/main.py
@@ -1,5 +1,13 @@
-print("ahmed ramadan") 
-print("ahmed ramadan")
-print("hellooooo")
-print("new line")
-print("new fucken line")
+#from cmath import sqrt
+#def second_degree(a,b,c,x):
+#    if x>1:
+#        print("should x ==1 ")
+#        
+#    else:
+#        v=(b**2)-(4*a*c)
+#        d=(-b-sqrt(v))/(2*a)
+#        x =(-b+sqrt(v))/(2*a)
+#        print(f"the first solution is '{x}'\nthe second solution is '{d}'")
+#second_degree(5,5,6,1)
+#
+    
